b17b2274cb4b80366347fe24e1da2b9ab306fa84
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-05-12 13:16:06 +1000

    preparing vscode for prog session

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12MayProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
